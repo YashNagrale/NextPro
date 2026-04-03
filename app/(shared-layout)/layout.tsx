dd8440000adac743f0dbe85bbcd87ef7283e6865
YashNagrale <ynagrale668@gmail.com>
2026-04-03 18:22:09 +0530

    fix: add dynamic export to layout for force-dynamic rendering

--- app/(shared-layout)/layout.tsx
+++ app/(shared-layout)/layout.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components/web/navbar";
 import { ReactNode } from "react";
+export const dynamic = "force-dynamic";
 
 export default function SharedLayout({ children }: { children: ReactNode }) {
   return (
