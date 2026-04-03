00e4247ccca682e78b76e2b3ec22ad569e967a03
YashNagrale <ynagrale668@gmail.com>
2026-04-03 19:56:57 +0530

    fix: update siteUrl to use NEXT_PUBLIC_SITE_URL for better compatibility

--- convex/auth.ts
+++ convex/auth.ts
@@ -9,7 +9,7 @@ import { betterAuth } from "better-auth/minimal";
 import authConfig from "./auth.config";
 
 // const siteUrl = process.env.SITE_URL!;
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL!;
+const siteUrl = process.env.SITE_URL!;
 
 // The component client has methods needed for integrating Convex with Better Auth,
 // as well as helper methods for general use.
