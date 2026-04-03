4087d1c863ef10068a04448f89e7fb40f999e852
YashNagrale <ynagrale668@gmail.com>
2026-04-03 19:11:04 +0530

    fix: add missing line for dynamic export in BlogPage component

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -7,6 +7,7 @@ import { Suspense } from "react";
 
 // export const dynamic = "force-static";
 // export const revalidate = 30;
+
 export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
