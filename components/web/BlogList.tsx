a490b8a25eb4460122d6cf172e3232d2214b9a4d
YashNagrale <ynagrale668@gmail.com>
2026-04-03 19:17:21 +0530

    fix: remove unused connection import and commented-out database call in BlogList component

--- components/web/BlogList.tsx
+++ components/web/BlogList.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import { buttonVariants } from "../ui/button";
 import { Skeleton } from "../ui/skeleton";
 import { Doc } from "@/convex/_generated/dataModel";
-import { connection } from "next/server";
 
 type PostWithImageUrl = Doc<"posts"> & { imageUrl: string | null };
 
@@ -17,8 +16,6 @@ export default async function BlogList({
   "use cache";
   cacheLife("hours");
   cacheTag("blog");
-
-  // await connection();
 
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
